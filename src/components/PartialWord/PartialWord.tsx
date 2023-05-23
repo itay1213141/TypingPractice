9fbc95a771479e22fb5acfafac44b26117073996
Itay Habani <itayhabani1213141@gmail.com>
2023-05-23 22:47:07 +0300

    Seperated a partial word to its own component

---
+++ src/components/PartialWord/PartialWord.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface IPartialWordProps {
+    word: string;
+    currentIndex: number;
+}
+
+const PartialWord: React.FC<IPartialWordProps> = ({ word, currentIndex }) => {
+  const start = word.substring(0, currentIndex);
+  const end = word.substring(currentIndex, word.length);
+
+  return (
+    <p className='partial-word'>
+      <span className='start'>{start}</span><span className='end'>{end}</span>
+    </p>
+  )
+}
+
+export default PartialWord
